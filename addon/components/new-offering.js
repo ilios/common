8b812b48b7908d801dad8a73d704210eea6c409e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-10-07 10:40:57 -0700

    when opening new offering form, scroll to its bottom.

--- addon/components/new-offering.js
+++ addon/components/new-offering.js
@@ -1,6 +1,7 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import layout from '../templates/components/new-offering';
+import scrollIntoView from 'scroll-into-view';
 
 export default Component.extend({
   store: service(),
@@ -10,6 +11,10 @@ export default Component.extend({
   courseStartDate: null,
   courseEndDate: null,
   smallGroupMode: true,
+  didRender() {
+    this._super(...arguments);
+    scrollIntoView(this.element.querySelector('.offering-form .buttons'));
+  },
   actions: {
     save(startDate, endDate, room, learnerGroups, instructorGroups, instructors){
       const store = this.get('store');
