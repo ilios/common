20128685976bfda620c8884e2e94800380ad378e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-11-04 14:16:34 -0800

    removes agenda view from dashboard.

--- tests/integration/components/dashboard-view-picker-test.js
+++ tests/integration/components/dashboard-view-picker-test.js
@@ -3,12 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | dashbaord view picker', function(hooks) {
+module('Integration | Component | dashboard view picker', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     const week = '[data-test-glance]';
-    const activities = '[data-test-activities';
     const materials = '[data-test-materials]';
     const calendar = '[data-test-calendar]';
 
@@ -18,8 +17,6 @@ module('Integration | Component | dashbaord view picker', function(hooks) {
 
     assert.dom(week).hasText('Week at a Glance');
     assert.dom(week).hasClass('active');
-    assert.dom(activities).hasText('Activities');
-    assert.dom(activities).hasNoClass('active');
     assert.dom(materials).hasText('Materials');
     assert.dom(materials).hasNoClass('active');
     assert.dom(calendar).hasText('Calendar');
@@ -28,7 +25,6 @@ module('Integration | Component | dashbaord view picker', function(hooks) {
 
   test('changing show changes active button', async function(assert) {
     const week = '[data-test-glance]';
-    const activities = '[data-test-activities';
     const materials = '[data-test-materials]';
     const calendar = '[data-test-calendar]';
 
@@ -37,25 +33,16 @@ module('Integration | Component | dashbaord view picker', function(hooks) {
     await render(hbs`<DashboardViewPicker @show={{show}} @change={{action nothing}} />`);
 
     assert.dom(week).hasClass('active');
-    assert.dom(activities).hasNoClass('active');
-    assert.dom(materials).hasNoClass('active');
-    assert.dom(calendar).hasNoClass('active');
-
-    this.set('show', 'agenda');
-    assert.dom(week).hasNoClass('agenda');
-    assert.dom(activities).hasClass('active');
     assert.dom(materials).hasNoClass('active');
     assert.dom(calendar).hasNoClass('active');
 
     this.set('show', 'materials');
-    assert.dom(week).hasNoClass('agenda');
-    assert.dom(activities).hasNoClass('active');
+    assert.dom(week).hasNoClass('active');
     assert.dom(materials).hasClass('active');
     assert.dom(calendar).hasNoClass('active');
 
     this.set('show', 'calendar');
-    assert.dom(week).hasNoClass('agenda');
-    assert.dom(activities).hasNoClass('active');
+    assert.dom(week).hasNoClass('active');
     assert.dom(materials).hasNoClass('active');
     assert.dom(calendar).hasClass('active');
   });
@@ -67,25 +54,11 @@ module('Integration | Component | dashbaord view picker', function(hooks) {
     this.set('click', what => {
       assert.equal(what, 'week');
     });
-    this.set('show', 'agenda');
+    this.set('show', 'materials');
     await render(hbs`<DashboardViewPicker @show={{show}} @change={{action click}} />`);
 
     assert.dom(week).hasNoClass('active');
     find(week).click();
-  });
-
-  test('clicking activities fires action', async function(assert) {
-    assert.expect(2);
-    const activities = '[data-test-activities';
-
-    this.set('click', what => {
-      assert.equal(what, 'agenda');
-    });
-    this.set('show', 'materials');
-    await render(hbs`<DashboardViewPicker @show={{show}} @change={{action click}} />`);
-
-    assert.dom(activities).hasNoClass('active');
-    find(activities).click();
   });
 
   test('clicking materials fires action', async function(assert) {
@@ -95,7 +68,7 @@ module('Integration | Component | dashbaord view picker', function(hooks) {
     this.set('click', what => {
       assert.equal(what, 'materials');
     });
-    this.set('show', 'agenda');
+    this.set('show', 'calendar');
     await render(hbs`<DashboardViewPicker @show={{show}} @change={{action click}} />`);
 
     assert.dom(materials).hasNoClass('active');
@@ -109,7 +82,7 @@ module('Integration | Component | dashbaord view picker', function(hooks) {
     this.set('click', what => {
       assert.equal(what, 'calendar');
     });
-    this.set('show', 'agenda');
+    this.set('show', 'materials');
     await render(hbs`<DashboardViewPicker @show={{show}} @change={{action click}} />`);
 
     assert.dom(calendar).hasNoClass('active');
