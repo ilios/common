c515c7675bd81361db92472804d5238f8b903aab
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-07-25 16:29:58 -0700

    replaces tasks with Resources/getters.

--- addon/components/course-visualize-instructor.js
+++ addon/components/course-visualize-instructor.js
@@ -1,24 +1,44 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import { restartableTask } from 'ember-concurrency';
 import { map, filter } from 'rsvp';
+import { use } from 'ember-could-get-used-to-this';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
+import AsyncProcess from 'ilios-common/classes/async-process';
 
 export default class CourseVisualizeInstructorComponent extends Component {
   @service iliosConfig;
-  @tracked academicYearCrossesCalendarYearBoundaries = false;
-  @tracked ilmMinutes = 0;
-  @tracked offeringMinutes = 0;
 
-  @restartableTask
-  *load() {
-    const sessions = yield this.args.course.sessions;
-    const sessionsWithUser = yield filter(sessions.toArray(), async (session) => {
-      const instructors = await session.getAllInstructors();
-      return instructors.mapBy('id').includes(this.args.user.id);
-    });
+  @use academicYearCrossesCalendarYearBoundaries = new ResolveAsyncValue(() => [
+    this.iliosConfig.itemFromConfig('academicYearCrossesCalendarYearBoundaries'),
+  ]);
 
-    const minutes = yield map(sessionsWithUser, async (session) => {
+  @use sessions = new ResolveAsyncValue(() => [this.args.course.sessions]);
+  @use sessionsWithUser = new AsyncProcess(() => [
+    this.getSessionsWithUser.bind(this),
+    this.sessions,
+  ]);
+
+  @use minutes = new AsyncProcess(() => [this.getMinutes.bind(this), this.sessionsWithUser]);
+
+  get totalInstructionalTime() {
+    if (!this.minutes) {
+      return 0;
+    }
+    return this.minutes.mapBy('offeringMinutes').reduce((total, mins) => total + mins, 0);
+  }
+
+  get totalIlmTime() {
+    if (!this.minutes) {
+      return 0;
+    }
+    return this.minutes.mapBy('ilmMinutes').reduce((total, mins) => total + mins, 0);
+  }
+
+  async getMinutes(sessionsWithUser) {
+    if (!sessionsWithUser) {
+      return [];
+    }
+    return map(sessionsWithUser, async (session) => {
       const offeringMinutes = await session.getTotalSumOfferingsDurationByInstructor(
         this.args.user
       );
@@ -28,13 +48,15 @@ export default class CourseVisualizeInstructorComponent extends Component {
         ilmMinutes,
       };
     });
-    this.offeringMinutes = minutes
-      .mapBy('offeringMinutes')
-      .reduce((total, mins) => total + mins, 0);
-    this.ilmMinutes = minutes.mapBy('ilmMinutes').reduce((total, mins) => total + mins, 0);
+  }
 
-    this.academicYearCrossesCalendarYearBoundaries = yield this.iliosConfig.itemFromConfig(
-      'academicYearCrossesCalendarYearBoundaries'
-    );
+  async getSessionsWithUser(sessions) {
+    if (!sessions) {
+      return [];
+    }
+    return filter(sessions.toArray(), async (session) => {
+      const instructors = await session.getAllInstructors();
+      return instructors.mapBy('id').includes(this.args.user.id);
+    });
   }
 }
